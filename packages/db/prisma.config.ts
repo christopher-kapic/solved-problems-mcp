a0248d125903d75fe08057559987caa053d8876b
Christopher Kapic <christopherkapic@gmail.com>
2026-02-27 16:24:47 -0600

    This should fix the docker build

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "node:path";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 dotenv.config({
   path: "../../apps/server/.env",
@@ -12,6 +12,6 @@ export default defineConfig({
     path: path.join("prisma", "migrations"),
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL,
   },
 });
